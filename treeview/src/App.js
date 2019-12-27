ab531b24b1451a494d14f06413c513359c9669cd
Darjotri <darojos1997@gmail.com>
2019-12-27 03:44:59 -0500

    Entrega primer parcial

--- treeview/src/App.js
+++ treeview/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Twitter Dissaster Classifier</h1>
         </header>
         <div className='container'>
